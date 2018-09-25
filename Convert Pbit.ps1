ec8de68e2d8493d2d5f89b6d6c177c56f717cedc
Pieter Van Den Bossche <pvdb@UF.be>
2018-09-25 21:05:32 +0200

    Refactoring
    Added parameter + refactoring

--- Convert Pbit.ps1
+++ Convert Pbit.ps1
@@ -1,45 +1,48 @@
+param([String]$Path)
+
 #Specify files with json data
 $jsonFileNames = "DataModelSchema", "DiagramState", "Report\Layout"
 
 #Get all .pbit files for a folder/repository
-$pbitFiles = Get-ChildItem -Filter *.pbit -Recurse
+$pbitFiles = Get-ChildItem -Path $Path -Filter *.pbit -Recurse
 
 #Rename and format files for each .pbit files
 foreach ($pbitFile in $pbitFiles)
 {  
-    $path = "$($pbitFile.DirectoryName)\$($pbitFile.BaseName)"
+    $location = "$($pbitFile.DirectoryName)\$($pbitFile.BaseName)"
 
-    Copy-Item $pbitFile.FullName -Destination "$($path).zip"
-    Expand-Archive -Path "$($path).zip" -DestinationPath "$($path)" -Force
+    Copy-Item $pbitFile.FullName -Destination "$($location).zip"
+    Expand-Archive -Path "$($location).zip" -DestinationPath "$($location)" -Force
 
-    $text = Get-Content "$($path)\DataMashup"
+    $text = Get-Content "$($location)\DataMashup"
     $string = $text[4].ToString()
     $len = $string.IndexOf("/LocalPackageMetadataFile") - $string.IndexOf("LocalPackageMetadataFile")
-    $string.Substring($string.IndexOf("LocalPackageMetadataFile") -1, $len + 27) | Out-File -FilePath "$($path)\DataMashup.xml" -Encoding utf8
+    $string.Substring($string.IndexOf("LocalPackageMetadataFile") -1, $len + 27) | Out-File -FilePath "$($location)\DataMashup.xml" -Encoding utf8
 
     foreach($jsonFileName in $jsonFileNames)
     {
-        $file = Get-ItemProperty -Path "$($path)\$($jsonFileName)"
+        $file = Get-ItemProperty -Path "$($location)\$($jsonFileName)"
 
-        $text = Get-Content "$($path)\$($jsonFileName)" -Encoding Unknown
-        if(Test-Path -Path "$($path)\$($jsonFileName).json")
+        $text = Get-Content "$($location)\$($jsonFileName)" -Encoding Unknown
+        if(Test-Path -Path "$($location)\$($jsonFileName).json")
         {
-            Set-Content -Path "$($path)\$($jsonFileName).json" -Value $text -Encoding utf8 -Force
-            Remove-Item -Path "$($path)\$($jsonFileName)"
+            Set-Content -Path "$($location)\$($jsonFileName).json" -Value $text -Encoding utf8 -Force
+            Remove-Item -Path "$($location)\$($jsonFileName)"
         }
+
         else
         {
-            Set-Content -Path "$($path)\$($jsonFileName)" -Value $text -Encoding utf8 -Force
-            Rename-Item -Path "$($path)\$($jsonFileName)" -NewName "$($file.BaseName).json"
+            Set-Content -Path "$($location)\$($jsonFileName)" -Value $text -Encoding utf8 -Force
+            Rename-Item -Path "$($location)\$($jsonFileName)" -NewName "$($file.BaseName).json"
         }
 
     }
 
     #Remove unnecessary files
-    Remove-Item -Path "$($path).zip"
-    Remove-Item -Path "$($path)\DataMashup"
-    Remove-Item -Path "$($path)\Metadata"
-    Remove-Item -Path "$($path)\SecurityBindings"
-    Remove-Item -Path "$($path)\Settings"
-    Remove-Item -Path "$($path)\Version"
+    Remove-Item -Path "$($location).zip"
+    Remove-Item -Path "$($location)\DataMashup"
+    Remove-Item -Path "$($location)\Metadata"
+    Remove-Item -Path "$($location)\SecurityBindings"
+    Remove-Item -Path "$($location)\Settings"
+    Remove-Item -Path "$($location)\Version"
 }
